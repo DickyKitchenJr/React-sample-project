96942422fa4b2338e56b491d4a830a19cb5b39b0
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-23 16:28:14 -0400

    trying to fix image problem

--- amazon-best-seller/src/App.js
+++ amazon-best-seller/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faFlagUsa } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function App() {
   const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} />;
+  const flagIcon = <FontAwesomeIcon icon={faFlagUsa} />;
+  const shoppingCart = <FontAwesomeIcon icon={faCartShopping} />;
   return (
     <div className="App">
       <header>
-        <img src="images/amazonlogo.jpg" alt="amazon logo" />
         {/* NOT DISPLAYING */}
+        <img
+          src="amazon-best-seller\src\images\amazonlogo.jpg"
+          alt="amazon logo"
+        />
         <p>
           hello
           <br />
@@ -21,6 +28,18 @@ function App() {
             {searchIcon}
           </button>
         </form>
+        <p>{flagIcon}EN</p>
+        <p>
+          Hello, Sign in
+          <br />
+          <span>Account & Lists</span>
+        </p>
+        <p>
+          Returns
+          <br />
+          <span>& Orders</span>
+        </p>
+        <p>{shoppingCart}Cart</p>
       </header>
     </div>
   );
