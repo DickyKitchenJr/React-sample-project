4f92409e6a14afce7671811945c8ebcf659bc647
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-23 15:33:10 -0400

    working on header

--- amazon-best-seller/src/App.js
+++ amazon-best-seller/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img
-          src=".images/amazonlogo.jpg"
-          alt="amazon logo"
-        />
+        <img src="images/amazonlogo.jpg" alt="amazon logo" />
+        <p>
+          hello
+          <br />
+          select your address
+        </p>
+        <form className="form">
+          <input type="search" placeholder="Search" className="search-field" />
+          <button type="submit" className="search-button">
+            <img src="images/search-icon.png" alt="search icon" />
+          </button>
+        </form>
       </header>
     </div>
   );
