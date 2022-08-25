b6611e1050fe77f0389ac35b94d9e1f95add94d4
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-25 15:26:02 -0400

    changed to component structure

--- amazon-best-seller/src/App.js
+++ amazon-best-seller/src/App.js
@@ -1,50 +1,9 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { faFlagUsa } from "@fortawesome/free-solid-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import TopBar from "./Components/TopBar";
 import "./App.css";
 
 function App() {
-  const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon"/>;
-  const flagIcon = <FontAwesomeIcon icon={faFlagUsa} className="flag-icon"/>;
-  const shoppingCart = <FontAwesomeIcon icon={faCartShopping}/>;
-  const locationDot = <FontAwesomeIcon icon={faLocationDot} />;
-  return (
-    <div className="App">
-      <header>
-        <img
-          className="amazon-logo"
-          src="/images/amazonlogo.png"
-          alt="amazon logo"
-        />
-        <p className="hello">
-          Hello
-          <br />
-          <span className="topbar span">{locationDot} Select Your Address</span>
-        </p>
-        <form className="form">
-          <input type="search" placeholder="Search" className="search-field" size="100"/>
-          <button type="submit" className="search-button">
-            {searchIcon}
-          </button>
-        </form>
-        <p className="topbar">English<br></br>{flagIcon}</p>
-        <p className="topbar">
-          Hello, Sign In
-          <br />
-          <span className="span">Account & Lists</span>
-        </p>
-        <p className="topbar">
-          Returns
-          <br />
-          <span className="span">& Orders</span>
-        </p>
-        <p className="topbar span cart">{shoppingCart}Cart</p>
-      </header>
-    </div>
-  );
+  return <TopBar />;
 }
 
 export default App;
