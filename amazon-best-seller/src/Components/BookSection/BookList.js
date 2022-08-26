6a396ca0002303a17b3d22869dbc440d8c1f7913
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-26 17:37:27 -0400

    fine tuning book list block

--- amazon-best-seller/src/Components/BookSection/BookList.js
+++ amazon-best-seller/src/Components/BookSection/BookList.js
@@ -6,21 +6,35 @@ import { faStarHalf } from "@fortawesome/free-solid-svg-icons";
 import "./BookList.css";
 
 function BookList() {
-    const star = <FontAwesomeIcon icon={faStar} className="star-icon" />;
-    const halfStar = <FontAwesomeIcon icon={faStarHalf} className="half-star-icon" />;
+  const star = <FontAwesomeIcon icon={faStar} className="star-icon" />;
+  const halfStar = (
+    <FontAwesomeIcon icon={faStarHalf} className="half-star-icon" />
+  );
   return (
     <div className="booklist">
       <p className="ranking-block">
         #1&nbsp;&nbsp;<span className="border-cap">&nbsp;&nbsp;&nbsp;</span>
       </p>
-      <img className="book-cover" src="https://m.media-amazon.com/images/I/811YQ9+UOOL._AC_UY327_FMwebp_QL65_.jpg" alt="book cover" />
-      <p className="book-title">Prey/Pray: Origin of The Average Man</p>
-      <p className="author">&gt;Dicky Kitchen Jr</p>
-      <div className="star-section">{star}{star}{star}{star}{halfStar}<p className="rating-amount">{randomNum()}</p>
+      <div className="inside-book-box">
+        <img
+          className="book-cover"
+          src="https://m.media-amazon.com/images/I/811YQ9+UOOL._AC_UY327_FMwebp_QL65_.jpg"
+          alt="book cover"
+        />
+        <p className="book-title">Prey/Pray: Origin of The Average Man</p>
+        <p className="author">&gt;Dicky Kitchen Jr</p>
+        <div className="star-section">
+          {star}
+          {star}
+          {star}
+          {star}
+          {halfStar}
+          <p className="rating-amount">{randomNum()}</p>
+        </div>
+
+        <p className="book-format-type">Paperback</p>
+        <p className="book-price">$14.99</p>
       </div>
-      
-      <p className="book-format-type">Paperback</p>
-      <p className="book-price">$14.99</p>
     </div>
   );
 }
