7c4efcedf860e0c4125208f6168bf97ecc3bab17
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-26 17:40:06 -0400

    Update BookSection.js

--- amazon-best-seller/src/Components/BookSection/BookSection.js
+++ amazon-best-seller/src/Components/BookSection/BookSection.js
@@ -6,14 +6,33 @@ function BookSection() {
   return (
     <div className="book-section">
       <p className="book-list-heading">Best Sellers in Thrillers & Suspense</p>
-      <div className='listed-books'>
-        <BookList  />
-        <BookList  />
-        <BookList  />
-        <BookList  />
-        <BookList  />
+      <div className="listed-books">
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
       </div>
-      
     </div>
   );
 }
