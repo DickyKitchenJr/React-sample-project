2755ee849c175f740783243bb83b00e212795216
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-27 11:13:01 -0400

    Update books.js

--- amazon-best-seller/src/Components/BookSection/books.js
+++ amazon-best-seller/src/Components/BookSection/books.js
@@ -10,17 +10,225 @@ export const books = [
 
   {
     id: 2,
-    img: "https://m.media-amazon.com/images/I/51Y8BNkUDDL.jpg",
-    title: "Orphan X",
-    author: "Gregg Hurwitz",
-    price: "$9.99"
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ag5ftoMQL._SX310_BO1,204,203,200_.jpg",
+    title: "Where Ravens Roost",
+    author: "Karin Nordin",
+    price: "$12.99",
   },
 
   {
     id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL._AC_UL600_SR600,400_.jpg",
+    title: "Verity",
+    author: "Colleen Hoover",
+    price: "$11.99",
+  },
+
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41SJyjmjZjL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Wealth of Time",
+    author: "Andre Gonzalez",
+    price: "$14.99",
+  },
+
+  {
+    id: 5,
+    img: "https://m.media-amazon.com/images/I/51Y8BNkUDDL.jpg",
+    title: "Orphan X",
+    author: "Gregg Hurwitz",
+    price: "$9.99",
+  },
+
+  {
+    id: 6,
+    img: "https://images-na.ssl-images-amazon.com/images/I/31XTs9XzuNL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "The Devil's Cohort: The Vampire's Vault: Book 1",
+    author: "Stephen Mills",
+    price: "$13.99",
+  },
+
+  {
+    id: 7,
     img: "https://m.media-amazon.com/images/I/71WxDHg3E-L._AC_UL480_FMwebp_QL65_.jpg",
     title: "Prey/Pray: Hunting Party - The Nurse",
     author: "Dicky Kitchen Jr",
-    price: "$4.99"
+    price: "$4.99",
+  },
+
+  {
+    id: 8,
+    img: "https://m.media-amazon.com/images/I/41ib8sqZToL._SY346_.jpg",
+    title: "Deadly Obsession: Mason Investigations Book 1",
+    author: "Sarah O'Neill",
+    price: "$14.99",
+  },
+
+  {
+    id: 9,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 10,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 12,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 13,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 14,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 15,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 16,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 17,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 18,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 19,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 20,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 21,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 22,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 23,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 24,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 25,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 26,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 27,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 28,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 29,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
+  },
+
+  {
+    id: 30,
+    img: "",
+    title: "",
+    author: "",
+    price: "$",
   },
 ];
