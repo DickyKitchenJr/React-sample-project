0b86c37a71ff806447f531307219b305bac1cffe
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-27 09:13:07 -0400

    Update books.js

--- amazon-best-seller/src/Components/BookSection/books.js
+++ amazon-best-seller/src/Components/BookSection/books.js
@@ -5,17 +5,22 @@ export const books = [
     title:
       "Prey/Pray: Origin of The Average Man: second edition with bonus material",
     author: "Dicky Kitchen Jr",
+    price: "$9.99",
   },
+
   {
     id: 2,
-    img: "https://m.media-amazon.com/images/I/71WxDHg3E-L._AC_UL480_FMwebp_QL65_.jpg",
-    title: "Prey/Pray: Hunting Party - The Nurse",
-    author: "Dicky Kitchen Jr",
-  },
-  {
-    id: 3,
     img: "https://m.media-amazon.com/images/I/51Y8BNkUDDL.jpg",
     title: "Orphan X",
     author: "Gregg Hurwitz",
+    price: "$9.99"
+  },
+
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/71WxDHg3E-L._AC_UL480_FMwebp_QL65_.jpg",
+    title: "Prey/Pray: Hunting Party - The Nurse",
+    author: "Dicky Kitchen Jr",
+    price: "$4.99"
   },
 ];
