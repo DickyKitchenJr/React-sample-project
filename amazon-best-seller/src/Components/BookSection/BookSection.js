139c9766b4f1d65f771107179eb4d9f212167d93
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-29 17:16:14 -0400

    finished bottom of booksection

--- amazon-best-seller/src/Components/BookSection/BookSection.js
+++ amazon-best-seller/src/Components/BookSection/BookSection.js
@@ -8,10 +8,16 @@ function BookSection() {
     <div className="book-section">
       <p className="book-list-heading">Best Sellers in Thrillers & Suspense</p>
       <div className="listed-books">
-        {books.map((book) =>{
-          return <BookList key ={book.id} {...book}></BookList>
+        {books.map((book) => {
+          return <BookList key={book.id} {...book}></BookList>;
         })}
       </div>
+      <div className='mock-page-buttons'><p>
+        &#8592; Previous Page <button className='current-page'>1</button> 
+        <button className='other-pages'>2</button> 
+        <button className='other-pages'>Next Page &#8594;</button>
+      </p></div>
+      
     </div>
   );
 }
