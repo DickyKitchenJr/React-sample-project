beee1d2e60eeb586032a4a7edeec5af48dd452e5
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-30 16:45:09 -0400

    finished MarketFooter li, fine tuning looks

--- amazon-best-seller/src/Components/MarketingFooter.js
+++ amazon-best-seller/src/Components/MarketingFooter.js
@@ -7,7 +7,7 @@ function MarketingFooter() {
       <ul className="list-column">
         <li className="list-title">Get to Know Us</li>
         <li>Careers</li>
-        <li>Amazon Newsletter</li>
+        <li className='joined-li'>Amazon<br/>Newsletter</li>
         <li>About Amazon</li>
         <li>Sustainability</li>
         <li>Press Center</li>
@@ -22,15 +22,15 @@ function MarketingFooter() {
         <li>Supply to Amazon</li>
         <li>Become an Affiliate</li>
         <li>Become a Delivery Driver</li>
-        <li>Start a package delivery business</li>
+        <li className='joined-li'>Start a package delivery<br/>business</li>
         <li>Advertise Your Products</li>
         <li>Self-Publish with Us</li>
         <li>Host an Amazon Hub</li>
-        <li>&gt; See More Ways to Make Money</li>
+        <li className='joined-li'>&gt; See More Ways to Make<br/>Money</li>
       </ul>
       <ul className="list-column">
-        <li className='list-title'>Amazon Payment Products</li>
-        <li>Amazon Rewards Visa Signature Cards</li>
+        <li className="list-title">Amazon Payment Products</li>
+        <li className='joined-li'>Amazon Rewards Visa Signature<br/>Cards</li>
         <li>Amazon Store Card</li>
         <li>Amazon Secured Card</li>
         <li>Amazon Business Card</li>
@@ -38,6 +38,19 @@ function MarketingFooter() {
         <li>Credit Card Marketplace</li>
         <li>Reload Your Balance</li>
         <li>Amazon Currency Converter</li>
+      </ul>
+      <ul className="list-column">
+        <li className='list-title'>Let Us Help You</li>
+        <li className='joined-li'>Amazon and COVID-<br/>19</li>
+        <li>Your Account</li>
+        <li>Your Orders</li>
+        <li className='joined-li'>Shipping Rates &<br/>Policies</li>
+        <li>Amazon Prime</li>
+        <li className='joined-li'>Returns &<br/>Replacements</li>
+        <li className='joined-li'>Manage Your<br/>Content and Devices</li>
+        <li className='joined-li'>Your Recalls and<br/>Product Safety<br/>Alerts</li>
+        <li>Amazon Assistant</li>
+        <li>Help</li>
       </ul>
     </div>
   );
