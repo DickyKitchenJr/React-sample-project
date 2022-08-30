9d8b7fcb65e1e64b93df5dd62c13ab0685e58253
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-08-30 14:48:37 -0400

    Update FooterBars.js

--- amazon-best-seller/src/Components/FooterBars.js
+++ amazon-best-seller/src/Components/FooterBars.js
@@ -7,7 +7,7 @@ function FooterBars() {
         <div className='sign-in-footer'>
             <p>See personalized recommendations</p>
             <button type='button' className='sign-in-button'>Sign in</button>
-            <p>New customer? <span>Start here.</span></p>
+            <p className='new-customer-text'>New customer? <span className='start-here'>Start here.</span></p>
         </div>
     </div>
   )
