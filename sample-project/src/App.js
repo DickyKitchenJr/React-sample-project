e3c08f82b980e9cab2aabda2bfe11cf7978742bf
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-08 08:54:05 -0400

    Update App.js

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import smile from './smile.png'
-import Banner from './Components/Banner';
-import Buttons from './Components/Buttons';
-import PageColor from './Components/PageColorChanger/PageColor'
+import "./App.css";
+import smile from "./smile.png";
+import Banner from "./Components/Banner";
+import Buttons from "./Components/Buttons";
+import PageColor from "./Components/PageColorChanger/PageColor";
 
 function App() {
   return (
@@ -11,19 +11,8 @@ function App() {
       <h2>Spin The Smile</h2>
       <Buttons />
       <header className="App-header">
-        <img src={smile} className="App-logo" alt="logo" />
+        {/* <img src={smile} className="App-logo" alt="logo" /> */}
         <PageColor />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
