66e178dc78806467efc66f41c0743afe545410e3
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-10-31 11:22:06 -0400

    improved styling

--- sample-project1/src/App.js
+++ sample-project1/src/App.js
@@ -4,26 +4,31 @@ import PageColor from "./PageColorChanger/PageColor";
 import "./App.css";
 
 function App() {
-  const [spin, setSpin] = useState(0)
+  const [spin, setSpin] = useState(0);
 
   const handleClick = () => {
-    if(spin === 0){
-      setSpin(1)
+    if (spin === 0) {
+      setSpin(1);
     } else {
-      setSpin(0)
+      setSpin(0);
     }
-  }
+  };
 
   return (
     <div className="App">
+
       <header className="App-header">
         <h1>React Experiments</h1>
         <p>Sometimes you just need a place to play around.</p>
       </header>
-      <img spin={spin} src={smile} className="App-logo" alt="smile face" />
 
-      <button onClick={handleClick}>Change Spin Direction</button>
+      <div className="smileSpin">
+        <img spin={spin} src={smile} className="App-logo" alt="smile face" />
+        <button onClick={handleClick}>Change Spin Direction</button>
+      </div>
+
       <PageColor />
+
     </div>
   );
 }
