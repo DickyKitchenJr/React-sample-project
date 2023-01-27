9794be31976e880e8837b30b78059dba74d2c584
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-27 09:33:00 -0500

    Update Form.test.js

--- sample-project1/src/__test__/Form.test.js
+++ sample-project1/src/__test__/Form.test.js
@@ -1,4 +1,4 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, queryByTestId } from "@testing-library/react";
 import Form from "../Form/Form";
 
 describe(Form, () => {
@@ -43,5 +43,25 @@ describe(Form, () => {
         expect(firstNameEntry).toBe("John");
         expect(lastNameEntry).toBe("Smith");
         expect(ageEntry).toBe("21");
+    })
+
+    it('deletes the values when the X button is clicked', () => {
+        const { getByTestId, getByRole, queryByTestId } = render(<Form />);
+        const firstName = getByTestId("firstName");
+        const lastName = getByTestId("lastName");
+        const Age = getByTestId("Age");
+        fireEvent.change(firstName, { target: { value: "John" } });
+        fireEvent.change(lastName, { target: { value: "Smith" } });
+        fireEvent.change(Age, { target: { value: "21" } });
+        const Add = getByRole("button", { name: "Add To List" });
+        fireEvent.click(Add);
+        const Delete = getByRole("button", {name: "X"});
+        fireEvent.click(Delete);
+        const firstNameEntry = queryByTestId("firstNameEntry");
+        const lastNameEntry = queryByTestId("lastNameEntry");
+        const ageEntry = queryByTestId('ageEntry');
+        expect(firstNameEntry).toBeEmptyDOMElement;
+        expect(lastNameEntry).toBeEmptyDOMElement;
+        expect(ageEntry).toBeEmptyDOMElement;
     })
 })
